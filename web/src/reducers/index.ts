db26f65cf3a5fdd7337dbb86c757544bc055058c
bitpredator <67551273+bitpredator@users.noreply.github.com>
2023-10-05 19:33:56 +0200

    feat: web\src\reducers\index.ts

---
+++ web/src/reducers/index.ts
@@ -0,0 +1,5 @@
+export { setupInventoryReducer } from './setupInventory';
+export { refreshSlotsReducer } from './refreshSlots';
+export { swapSlotsReducer } from './swapSlots';
+export { stackSlotsReducer } from './stackSlots';
+export { moveSlotsReducer } from './moveSlots';
